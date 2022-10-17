ba6e35ca70a24edb78dd205e7a073a784bf0333a
Ondra58 <97514734+Ondra58@users.noreply.github.com>
2022-10-17 09:16:36 +0200

    Add files via upload

---
+++ WindowsFormsApp15/Form1.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace WindowsFormsApp15
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+        DateTime cas1;
+        DateTime cas2;
+        private void button2_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = true;
+            button2.Enabled = false;
+            cas2 = DateTime.Now;
+            TimeSpan cas = cas2 - cas1;
+            MessageBox.Show(cas.Seconds.ToString());
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            button1.Enabled = false;
+            button2.Enabled = true;
+            cas1 = DateTime.Now;
+        }
+    }
+}
